17abb44de63616364c63eba6a631390f4077298a
contato12 <contato12araujo@gmail.com>
2023-01-04 22:18:30 -0300

    Estilo da página de detalhamento de contato.

--- config/data_process.php
+++ config/data_process.php
@@ -5,14 +5,39 @@
     include_once("connection.php");
     include_once("url.php");
 
-    $query = "SELECT * FROM contacts";
+    
 
-    $stmt = $conn->prepare($query);
+    $id;
 
-    $stmt->execute();
+    if(!empty($_GET)){
+        $id = $_GET["id"];
+    }
 
-    $contacts = [];
+    if(!empty($id)){
+        //Retorna os dados de um contato.
+        
+        $query = "SELECT * FROM contacts WHERE id=:id";
 
-    $contacts = $stmt->fetchAll(); 
-     
+        $stmt = $conn->prepare($query);
+
+        $stmt->bindParam(":id", $id);
+
+        $stmt->execute();
+
+        $contact = $stmt->fetch();
+
+    } else {
+        //Retorna todos os contatos.
+
+        $query = "SELECT * FROM contacts";
+
+        $stmt = $conn->prepare($query);
+    
+        $stmt->execute();
+    
+        $contacts = [];
+    
+        $contacts = $stmt->fetchAll(); 
+    }
+
 ?>
